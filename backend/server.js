58beed50fe5e9c5e8fa1ded8f6b4daeca03345c5
Deinennn <81692661+Yetiiii25@users.noreply.github.com>
2022-06-20 15:06:07 +0800

    make it better

--- backend/server.js
+++ backend/server.js
@@ -41,7 +41,7 @@ app.use(cookieParser());
 // app.use('/', express.static(path.join(__dirname, '/public')));
 
 // routes
-app.use('/', require('./routes/root'));
+// app.use('/', require('./routes/root'));
 app.use('/register', require('./routes/register'));
 app.use('/auth', require('./routes/auth'));
 app.use('/refresh', require('./routes/refresh'));
