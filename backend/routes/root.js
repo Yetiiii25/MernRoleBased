001e947824fa6b1298d10b2c1ce070f9ec9f57f8
Deinennn <81692661+Yetiiii25@users.noreply.github.com>
2022-06-20 14:54:59 +0800

    make it better

--- backend/routes/root.js
+++ backend/routes/root.js
@@ -2,14 +2,14 @@ const express = require('express');
 const router = express.Router();
 const path = require('path');
 
-// router.get('^/$|/index(.html)?', (req, res) => {
-//     res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
-// });
+router.get('^/$|/index(.html)?', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'views', 'index.html'));
+});
 
-router.get('*', (req, res) => {
-res.sendFile(
-    path.join(__dirname, '../', 'frontend', 'build', 'index.html')
-)
-})
+// router.get('*', (req, res) => {
+// res.sendFile(
+//     path.join(__dirname, '../', 'frontend', 'build', 'index.html')
+// )
+// })
 
 module.exports = router;
